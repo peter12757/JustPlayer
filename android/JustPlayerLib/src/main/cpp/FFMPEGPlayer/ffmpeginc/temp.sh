3fb171b72c618f6035ccd07c142bc8ad90272a86
xipeitao <xipeitao@yy.com>
2023-11-02 00:07:04 +0800

    flutter

--- android/JustPlayerLib/src/main/cpp/FFMPEGPlayer/ffmpeginc/temp.sh
+++
@@ -1,6 +0,0 @@
-currentpath=$(pwd)
-
-
-#sed -i "s/libavutil/\.\.\/libavutil/g" 'grep "libavutil" -rl $cacheFile/libavutil'
-#grep "\"libavutil\/" -rl ./libavutil
-grep "\"libavutil\/" -rl ./libavutil | xargs sed -i "" "s/\"libavutil\//\"\.\.\/libavutil\//g"
