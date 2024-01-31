3add075ca4ae3424d853b4498aa1683b35d1b52b
xipeitao <xipeitao@yy.com>
2024-01-31 17:31:36 +0800

    launcher activity

--- android/app/src/main/java/com/eathemeat/justplayer/launcher/LauncherActivity.kt
+++ android/app/src/main/java/com/eathemeat/justplayer/launcher/LauncherActivity.kt
@@ -1,18 +1,27 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.eathemeat.justplayer.launcher
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.TopEnd
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.eathemeat.justplayer.R
 import com.eathemeat.justplayer.ui.theme.JustPlayerTheme
 
@@ -35,8 +44,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(modifier: Modifier = Modifier) {
-    Image(painterResource(id = R.drawable.img_def_launcher),modifier = modifier, contentDescription = "default launcher image")
-    Text(text = "5S", color = Color(android.graphics.Color.WHITE))
+    ConstraintLayout {
+        Box {
+            Image(painterResource(id = R.drawable.img_def_launcher),modifier = modifier, contentDescription = "default launcher image")
+            Text(text = "3S",
+                Modifier.padding(all = 10.dp)
+                    .align(TopEnd)
+                ,color = Color(android.graphics.Color.WHITE)
+            )
+        }
+
+
+    }
+
 }
 
 @Preview(showBackground = true)
