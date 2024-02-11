042341d3ff40090110bbf80c06d68be8372b8dae
xipeitao <xipeitao@yy.com>
2024-02-11 14:09:01 +0800

    add launcher pic

--- android/app/src/main/java/com/eathemeat/justplayer/launcher/LauncherActivity.kt
+++ android/app/src/main/java/com/eathemeat/justplayer/launcher/LauncherActivity.kt
@@ -3,21 +3,27 @@
 package com.eathemeat.justplayer.launcher
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment.Companion.TopEnd
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -25,7 +31,8 @@ import androidx.constraintlayout.compose.ConstraintLayout
 import com.eathemeat.justplayer.R
 import com.eathemeat.justplayer.ui.theme.JustPlayerTheme
 
-class MainActivity : ComponentActivity() {
+class LauncherActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -42,15 +49,33 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+private val timeColorArr = arrayOf(Color.Red,Color.Black,Color.Blue, Color.Gray,Color.Yellow,Color.White)
+const val TAG = "LauncherActivity"
+
 @Composable
 fun Greeting(modifier: Modifier = Modifier) {
+
+    var curColor = timeColorArr.random()
+    var clickTimes by remember {
+        mutableIntStateOf(0)
+    }
+    Log.d(TAG, "Greeting: step1 clickTimes:${clickTimes} curColor:${curColor}")
+
+    //sedounds
     ConstraintLayout {
         Box {
-            Image(painterResource(id = R.drawable.img_def_launcher),modifier = modifier, contentDescription = "default launcher image")
-            Text(text = "3S",
-                Modifier.padding(all = 10.dp)
+            Image(painterResource(id = R.drawable.img_def_launcher)
+                ,modifier = modifier
+                , contentDescription = "default launcher image"
+            , contentScale = ContentScale.FillBounds)
+            Text(text = "${clickTimes}S",
+                Modifier
+                    .padding(all = 10.dp)
                     .align(TopEnd)
-                ,color = Color(android.graphics.Color.WHITE)
+                    .clickable {
+                        clickTimes++
+                    }
+                ,color = curColor
             )
         }
 
