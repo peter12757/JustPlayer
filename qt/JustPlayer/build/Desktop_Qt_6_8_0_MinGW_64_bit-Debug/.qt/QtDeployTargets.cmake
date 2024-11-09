2d0ff16a1885e1e47befbcdefa1b225566dbe974
PeterXi <peter12757@126.com>
2024-11-09 14:40:34 +0800

    add qt

---
+++ qt/JustPlayer/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_JustPlayer_FILE F:/github/JustPlayer/qt/JustPlayer/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/JustPlayer.exe)
+set(__QT_DEPLOY_TARGET_JustPlayer_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_JustPlayer_RUNTIME_DLLS E:/qt/6.8.0/mingw_64/bin/Qt6Widgets.dll;E:/qt/6.8.0/mingw_64/bin/Qt6Gui.dll;E:/qt/6.8.0/mingw_64/bin/Qt6Core.dll)
