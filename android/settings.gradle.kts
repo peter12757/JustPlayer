389251084996910f993ac1e7c1ce0a94704dfd1f
PeterXi <peter12757@126.com>
2026-03-09 10:48:13 +0800

    new

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,6 +1,7 @@
 pluginManagement {
     repositories {
         google()
+        maven { url=uri("https://maven.aliyun.com/nexus/content/groups/public/") }
         mavenCentral()
         gradlePluginPortal()
         mavenLocal()
@@ -10,6 +11,7 @@ dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
+        maven { url=uri("https://maven.aliyun.com/nexus/content/groups/public/") }
         mavenCentral()
         mavenLocal()
     }
